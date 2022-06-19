fef64b653109c5b5c1823b3919933d76f25227d7
Allan Kruger <allan.kruger@compasso.com.br>
2022-06-19 03:37:09 +0100

    adds header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./global.css";
+import styles from "./App.module.css";
+import Logo from "./assets/todologo.svg";
 
 export function App() {
   return (
     <div className="App">
-      <h1>Todo</h1>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="" />
+        </div>
+      </header>
+      <div className={styles.searchBar}>
+        <input type="text" />
+        <button>Create</button>
+      </div>
     </div>
   );
 }
