fbfd037145dbfb185ab61ea8e69bf04faf1a5286
Allan Kruger <allan.kruger@compasso.com.br>
2022-06-19 15:19:37 +0100

    finishes styling for no task page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./global.css";
 import styles from "./App.module.css";
 import Logo from "./assets/todologo.svg";
-import { PlusCircle } from "phosphor-react";
+import { PlusCircle, Clipboard } from "phosphor-react";
 
 export function App() {
   return (
@@ -17,6 +17,28 @@ export function App() {
           Create <PlusCircle size={18} weight="bold" />
         </button>
       </div>
+
+      <section className={styles.taskList}>
+        <div className={styles.taskListHeader}>
+          <div>
+            <strong className={styles.createdTasks}>
+              Created tasks<span>5</span>
+            </strong>
+          </div>
+
+          <div>
+            <strong className={styles.finishedTasks}>
+              Finished<span>5</span>
+            </strong>
+          </div>
+        </div>
+
+        <div className={styles.taskListBody}>
+          <Clipboard size={56} />
+          <strong>You don't have any tasks registered</strong>
+          <p>Create tasks and organize your to do items</p>
+        </div>
+      </section>
     </div>
   );
 }
