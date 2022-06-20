1831894cbfe30130e5019c1ee47c0f0da0359e3b
Allan Kruger <allan.kruger@compasso.com.br>
2022-06-20 20:29:15 +0100

    finishes styling

--- src/assets/components/List/index.tsx
+++ src/assets/components/List/index.tsx
@@ -6,17 +6,29 @@ import { useState } from "react";
 export function List() {
   const [isListEmpty, setIsListEmpty] = useState(false);
 
-  return (
-    isListEmpty ? (
-      <section className={styles.taskList}>
-        <div className={styles.taskListBody}>
-          <Clipboard size={56} />
-          <strong>You don't have any tasks registered</strong>
-          <p>Create tasks and organize your to do items</p>
-        </div>
-      </section>
-    ) : (
+  return isListEmpty ? (
+    <section className={styles.taskList}>
+      <div className={styles.taskListBody}>
+        <Clipboard size={56} />
+        <strong>You don't have any tasks registered</strong>
+        <p>Create tasks and organize your to do items</p>
+      </div>
+    </section>
+  ) : (
+    <>
       <ListItem />
-    )
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </>
   );
 }
--- src/assets/components/ListItem/index.tsx
+++ src/assets/components/ListItem/index.tsx
@@ -1,3 +1,15 @@
+import styles from "./styles.module.css";
+import { Trash } from "phosphor-react";
+
 export function ListItem() {
-  return <h1>List Item</h1>;
+  return (
+    <div className={styles.listItemContainer}>
+      <label>
+        <input type="checkbox" name="listItem" />
+        <p>Integer urna interdum massa libero auctor neque turpis turpis semper.
+        Duis vel sed fames integer.</p>
+      </label>
+      <Trash size={18} />
+    </div>
+  );
 }
